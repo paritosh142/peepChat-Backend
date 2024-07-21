a4bc854c7442dbcbe7e8d8d9ac045ce88689598f
Paritosh <79096636+paritosh142@users.noreply.github.com>
2024-07-21 14:14:01 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,10 @@
+import cors from "cors";
+
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://peep-chat-frontend.vercel.app/"],
+  origin: process.env.CLIENT_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 const PEEP_CHAT_TOKEN = "peepChaToken";
